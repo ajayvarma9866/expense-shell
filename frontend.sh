6d20d7da7fca30a24d02dc387784da61f82c52bb
ajayvarma <ajayvarma9866@gmail.com>
2023-11-15 23:44:34 +0530

    Adding Shell Topics

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,6 @@ cp expense.conf /etc/nginx/default.d/expense.conf
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
-
 unzip /temp/frontend.zip
 
 systemctl enable nginx
