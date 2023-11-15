d95f81a015d4f378ab675143c184ce8353ccbcde
ajayvarma <ajayvarma9866@gmail.com>
2023-11-15 23:37:16 +0530

    Adding Shell Topics

---
+++ frontend.sh
@@ -0,0 +1,16 @@
+dnf install nginx -y
+
+rm-rf /usr/share/nginx/html/*
+
+cp expense.conf /etc/nginx/default.d/expense.conf
+
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+
+unzip /temp/frontend.zip
+
+systemctl enable nginx
+systemctl restart nginx
+
+
