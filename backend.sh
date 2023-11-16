fc9b5bef452e8886bb8efacc59f9a7c11cc87b3e
ajayvarma <ajayvarma9866@gmail.com>
2023-11-16 16:30:55 +0530

    Adding Shell Topics

--- backend.sh
+++ backend.sh
@@ -4,55 +4,94 @@ colour="\e[36m"
 
 echo -e "${colour} disable nodejs default version \e[0m"
 dnf module disable nodejs -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} enable nodejs 18 version \e[0m"
 dnf module enable nodejs:18 -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} install nodejs \e[0m"
 dnf install nodejs -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} copy backend service file \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} add application user \e[0m"
 useradd expense &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} create application directory \e[0m"
 mkdir /app &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} delete old application content \e[0m"
 rm -rf /app/* &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} download application content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} extract application content \e[0m"
 cd /app
 unzip /tmp/backend.zip &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} download nodejs dependencies \e[0m"
 npm install &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} install mysql client to load schema \e[0m"
 dnf install mysql -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} load schema \e[0m"
 mysql -h mysql-dev.devops9866.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
 
 echo -e "${colour} starting backend service \e[0m"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "${colour} SUCCESS \e[0m"
+else
+  echo -e "${colour} FAILURE \e[0m"
