df56373a2ddda32b0e5569717fe0bb4c0f0b4cc0
ajayvarma <ajayvarma9866@gmail.com>
2023-11-16 18:39:25 +0530

    Adding Shell Topics

--- backend.sh
+++ backend.sh
@@ -1,6 +1,4 @@
-
-log_file=/tmp/expense.log
-colour="\e[36m"
+source common.sh
 
 if [ -z "$1" ]; then
   echo password input missing
@@ -8,13 +6,7 @@ if [ -z "$1" ]; then
 fi
 MYSQL_ROOT_PASSWORD=$1
 
-status_check(){
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32m SUCCESS \e[0m"
-  else
-    echo -e "\e[31m FAILURE \e[0m"
-  fi
-}
+
 echo -e "${colour} disable nodejs default version \e[0m"
 dnf module disable nodejs -y &>>$log_file
 status_check
--- mysql.sh
+++ mysql.sh
@@ -1,23 +1,28 @@
-log_file=/tmp/expense.log
-colour="\e[36m"
+source common.sh
+
+if [ -z "$1" ]; then
+  echo password input missing
+  exit
+fi
+MYSQL_ROOT_PASSWORD=$1
 
 echo -e "${colour} disable mysql default version \e[0m"
 dnf module disable mysql -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${colour} copy mysql repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-echo $?
+status_check
 
 echo -e "${colour} install mysql server \e[0m"
 dnf install mysql-community-server -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${colour} start mysql server \e[0m"
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
-echo $?
+status_check
 
 echo -e "${colour} set mysql password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-echo $?
+mysql_secure_installation --set-root-pass ${MYSQL_ROOT_PASSWORD} &>>$log_file
+status_check
