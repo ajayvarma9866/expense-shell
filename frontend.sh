276095322021ccfd86460ade6cffd7a852a95078
ajayvarma <ajayvarma9866@gmail.com>
2023-11-16 11:32:54 +0530

    Adding Shell Topics

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,19 @@
+echo -e "\e[36m installing nginx \e[0m"
 dnf install nginx -y
 
+echo -e "\e[36m adding configuration file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
+echo -e "\e[36m removing default data \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[36m download frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[36m extracting downloaded content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo -e "\e[36m starting nginx service \e[0m"
 systemctl enable nginx
 systemctl restart nginx
