8d5ec51028e78663ad8442502f05403b01e2ed8b
ajayvarma <ajayvarma9866@gmail.com>
2024-04-21 19:07:40 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -44,7 +44,7 @@ VALIDATE $? "starting MySQL server"
 
 #Below code will be useful for idempotent nature
 
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.devops9866.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
